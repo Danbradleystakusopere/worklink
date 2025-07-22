587ee5e6943ab7c972f7b64fb9bff446ab5eef06
Danbradleystakusopere <danstakus@gmail.com>
2025-07-22 19:55:17 +0300

    Update App.js to include JobForm component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import JobForm from "./components/JobForm";
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/jobs")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
+
+  function handleAddJob(newJob) {
+    setJobs((prevJobs) => [...prevJobs, newJob]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WorkLink - Remote Jobs Board</h1>
+      <JobForm onAddJob={handleAddJob} />
+      <ul>
+        {jobs.map((job) => (
+          <li key={job.id}>
+            <h3>{job.title}</h3>
+            <p>{job.description}</p>
+            <small>{job.category}</small>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
