d38159b79502483568d868b89ebf3edb101b99bf
Danbradleystakusopere <danstakus@gmail.com>
2025-07-23 12:43:20 +0300

    added light and dark mode

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,16 @@
-
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 
-function NavBar() {
+function NavBar({ darkMode, toggleDarkMode }) {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/proposals">All Proposals</Link>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/proposals">Proposals</Link>
+      </div>
+      <button onClick={toggleDarkMode}>
+        {darkMode ? "☀ Light Mode" : " Dark Mode"}
+      </button>
     </nav>
   );
 }
